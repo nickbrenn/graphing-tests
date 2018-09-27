9126886caac22ca0d10ccdde152af1d62e22221e
nickbrenn <nbren@tutanota.com>
2018-09-27 05:05:53 -0400

    Got dygraph to work with csv data from alphavantage but with hideous results

--- client/src/components/DataView.js
+++ client/src/components/DataView.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
+import Dygraph from "dygraphs";
 
 const apikey = require("../config/keys.json").alphavantage_apikey;
-const baseUrl = "https://www.alphavantage.co/query?"
+const baseUrl = "https://www.alphavantage.co/query?";
 
 class DataView extends Component {
   state = {
@@ -11,69 +12,83 @@ class DataView extends Component {
     function: "TIME_SERIES_INTRADAY",
     interval: "1min",
     outputsize: "compact",
-    datatype: "json",
+    datatype: "csv",
     data: null
-  }
+  };
 
   fetchData = () => {
     let fetchUrl = baseUrl;
-    this.state.symbol ? fetchUrl += `&symbol=${this.state.symbol}` : null;
-    this.state.function ? fetchUrl += `&function=${this.state.function}` : null;
-    this.state.interval ? fetchUrl += `&interval=${this.state.interval}` : null;
-    this.state.outputsize ? fetchUrl += `&outputsize=${this.state.outputsize}` : null;
-    this.state.datatype ? fetchUrl += `&datatype=${this.state.datatype}` : null;
-    apikey ? fetchUrl += `&apikey=${apikey}` : null;
+    this.state.symbol ? (fetchUrl += `&symbol=${this.state.symbol}`) : null;
+    this.state.function
+      ? (fetchUrl += `&function=${this.state.function}`)
+      : null;
+    this.state.interval
+      ? (fetchUrl += `&interval=${this.state.interval}`)
+      : null;
+    this.state.outputsize
+      ? (fetchUrl += `&outputsize=${this.state.outputsize}`)
+      : null;
+    this.state.datatype
+      ? (fetchUrl += `&datatype=${this.state.datatype}`)
+      : null;
+    apikey ? (fetchUrl += `&apikey=${apikey}`) : null;
 
-    axios
-      .get(fetchUrl)
-      .then(response => {
-        const keyNames = Object.keys(response.data);
-        const headData = response.data[keyNames[0]];
-        const jsonData = response.data[keyNames[1]];
-        const arr = Object.keys(jsonData).map((key) => [key, jsonData[key]]);
-        this.setState({ data:  arr  })
-      })
-      .catch(error => {
-        console.log("Error:", error);
-        this.setState({ error: "There was an error!" });
-      })
-
+    new Dygraph(this.refs.chart, fetchUrl, {
+      legend: "always"
+    });
+    // axios
+    //   .get(fetchUrl)
+    //   .then(response => {
+    //     const keyNames = Object.keys(response.data);
+    //     const headData = response.data[keyNames[0]];
+    //     const jsonData = response.data[keyNames[1]];
+    //     const arr = Object.keys(jsonData).map(key => [key, jsonData[key]]);
+    //     this.setState({ data: arr });
+    //   })
+    //   .catch(error => {
+    //     console.log("Error:", error);
+    //     this.setState({ error: "There was an error!" });
+    //   });
     console.log("fetchUrl = " + fetchUrl);
-  }
-  
+  };
+
   componentDidMount = () => {
     this.fetchData();
-  }
+  };
 
   render() {
-    if(this.state.data !== null){
+    if (this.state.data !== null) {
       return (
         <div>
           {this.state.data.map((datapoint, index) => {
             return (
               <div key={index}>
                 {datapoint[0]}
-                {Object.keys(datapoint[1]).map((key,index) => {
+                {Object.keys(datapoint[1]).map((key, index) => {
                   return (
-                    <div key={key+index}>
-                      {key}{": "}{datapoint[1][key]}
+                    <div key={key + index}>
+                      {key}
+                      {": "}
+                      {datapoint[1][key]}
                     </div>
-                  );  
+                  );
                 })}
+                {"================"}
               </div>
-            )
+            );
           })}
         </div>
-      )
+      );
     } else {
       return (
         <div className="DataView">
           <header className="DataView-header">
-            <h1 className="DataView-title">Welcome to React</h1>
+            <h1 className="DataView-title">Welcome to Graphing-test :)</h1>
           </header>
-          <p className="DataView-intro">
-            This is your api key: {apikey}
-          </p>
+          <p className="DataView-intro">This is your api key: {apikey}</p>
+          <div className="graph">
+            <div ref="chart" />
+          </div>
         </div>
       );
     }
